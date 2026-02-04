1c312ddf4fcafbf623f79f798ff9152d0675ccd7
avinilo <alvaroviniloo@gmail.com>
2026-02-04 15:38:36 +0100

    perf: make CSS non-render-blocking with preload async pattern

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sitemap from 'vite-plugin-sitemap'
 
+// Custom plugin to make CSS non-render-blocking
+const asyncCssPlugin = () => ({
+    name: 'async-css',
+    transformIndexHtml(html) {
+        // Transform CSS links to use async loading pattern
+        return html.replace(
+            /<link rel="stylesheet" crossorigin href="(\/assets\/[^"]+\.css)">/g,
+            `<link rel="preload" as="style" href="$1" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="$1"></noscript>`
+        );
+    }
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        asyncCssPlugin(),
         sitemap({
             hostname: 'https://eastglobal.es',
             dynamicRoutes: [
